6a9c67261822d1f38360bbb5f2dd0cb2b06eb015
Jerry <ylf989@163.com>
2017-09-27 14:28:01 +0800

    Signed-off-by: Jerry <ylf989@163.com>

--- com/ylf/file/FolderComparator.py
+++ com/ylf/file/FolderComparator.py
@@ -10,8 +10,9 @@ folder1 = r"C:\Users\e589831\Desktop\ssrp_97"
 folder2 = r"C:\Users\e589831\Desktop\ssrp_98"
 
 def scanFolder(mainFolder, toBeComparedFolder):
-    onlyInMainFolderFiles = differenctFiles = []
-    for dirName, subdirList, fileList in os.walk(mainFolder):
+    onlyInMainFolderFiles = []
+    differenctFiles = []
+    for dirName, _, fileList in os.walk(mainFolder):
         for fname in fileList:
             fullNameInMainFolder = dirName+"\\"+fname
             fullNameInToBeComparedFolder = dirName.replace(mainFolder, toBeComparedFolder)+"\\"+fname
@@ -26,20 +27,23 @@ def compareFolders(folder1, folder2):
     result1 = scanFolder(folder1, folder2)
     result2 = scanFolder(folder2, folder1)
     return (result1[0], result2[0], result1[1])
-    
-if __name__ == '__main__':
+
+def compareAndPrint():
     result = compareFolders(folder1, folder2)
     if len(result[0]) != 0:
-        print "-----OnlyInFolder1-----"
+        print "-----OnlyInFolder1Files-----"
         for i in result[0]:
             print i
     
     if len(result[1]) != 0:
-        print "-----OnlyInFolder2-----"
+        print "-----OnlyInFolder2Files-----"
         for i in result[1]:
             print i
     
     if len(result[2]) != 0:
-        print "-----DiffFiles-----"
+        print "-----DifferentFiles-----"
         for i in result[2]:
             print i
+    
+if __name__ == '__main__':
+    compareAndPrint()
